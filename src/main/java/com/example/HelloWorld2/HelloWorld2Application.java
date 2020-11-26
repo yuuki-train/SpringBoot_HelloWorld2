90227bff4b57d3c52638f8d85690487daafeed58
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-11-26 16:44:55 +0900

    HelloWorld2 first commit

---
+++ src/main/java/com/example/HelloWorld2/HelloWorld2Application.java
@@ -0,0 +1,25 @@
+package com.example.HelloWorld2;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorld2Application {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld2Application.class, args);
+	}
+
+}
